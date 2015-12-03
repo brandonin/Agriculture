d27771921e471b6f1388c176cf2a15872fd27c7b
brandon in <brandonin@me.com>
2015-12-02 16:28:29 -0800

    fixed routing issues

--- client/angular/config.js
+++ client/angular/config.js
@@ -1,10 +1,17 @@
-var app = angular.module('app', [
-	'ngRoute'
-	]);
+var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/',{
         templateUrl: 'partials/landing_page.html'
+    })
+    .when('/about',{
+        templateUrl: 'partials/about.html'
+    })
+    .when('/login',{
+        templateUrl: 'partials/dashboard.html'
+    })
+    .when('/register',{
+        templateUrl: 'partials/dashboard.html'
     })
 });
