deb5414bb463ea68a28130a64e241ebf9294f329
brandon in <brandonin@me.com>
2015-12-02 20:10:51 -0800

    added controller and factory skeleton

--- client/angular/DashboardController.js
+++ client/angular/DashboardController.js
@@ -1,3 +1,18 @@
-app.controller("DashboardController", function($scope, DashboardFactory) {
-
+app.controller("UserController", function($scope, UserFactory) {
+	UserFactory.getUsers(function (data) {
+		$scope.users = data;
+	});
+	$scope.addUser = function() {
+		UserFactory.addUser($scope.new_user, function(errs) {
+			if(errs){
+				$scope.errors = errs;
+			} else {
+			// else do the get customers stuff below
+			UserFactory.getUsers(function (output) {
+				$scope.users(output);
+				$scope.new_user = {};
+			});
+			}
+		});
+	};
 })
--- client/angular/DashboardFactory.js
+++ client/angular/DashboardFactory.js
@@ -1,6 +1,30 @@
-app.factory('DashboardFactory', function($http) {
+app.factory('UserFactory', function($http) {
 	var factory = {};
-	var tax = {};
-
+	var users = [];
+	var errors;
+	factory.getUsers = function(callback) {
+		$http.get('/users').success(function(output){
+			users = output;
+			callback(users);
+		});
+	};
+	factory.addUser = function(info, callback) {
+		var count = 0;
+		for(var user in users) {
+			if(info.name == users[user].name)
+			{
+				count++;
+			}
+		}
+		if(count>0) {
+			errors = {message: "Username already exists"};
+			callback(errors)
+		} else {
+			$http.post('/addUser', info).success(function (output) {
+				users.push({first_name: info.first_name, last_name: info.last_name, username: info.username, email: info.email, password: info.password, created_at: new Date()})
+			});
+		}
+		
+	};	
 	return factory;
 });
